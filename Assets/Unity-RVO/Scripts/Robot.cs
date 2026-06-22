b1f3ab15ceb4547988ed5e9ba7381cb57d481651
rkim-test <246917365+rkim-test@users.noreply.github.com>
2026-06-21 22:06:10 -0400

    robot now properly shows gizmos relative to parent transform, as positions in VO nativearrays are local space positions

--- Assets/Unity-RVO/Scripts/Robot.cs
+++ Assets/Unity-RVO/Scripts/Robot.cs
@@ -20,14 +20,14 @@ namespace RVO {
             if (!Application.isPlaying) return;
             if (generator == null) return;
             // Draw ourselves
-            Vector3 pA = generator.vo_op.positions[agent_index];
+            Vector3 pA = transform.parent.TransformPoint(generator.vo_op.positions[agent_index]);
             Gizmos.color = Color.black;
             Gizmos.DrawWireSphere(pA, personality.spatial_radius);
             // show neighbors
             Gizmos.color = generator.vo_op.colliding[agent_index] ? Color.red : Color.blue;
             for(int i = 0; i < generator.vo_op.num_neighbors[agent_index]; i++) {
                 int neighbor_index = generator.vo_op.neighbor_indices[agent_index*generator.max_neighbors+i];
-                Vector3 pB = generator.vo_op.positions[neighbor_index];
+                Vector3 pB = transform.parent.TransformPoint(generator.vo_op.positions[neighbor_index]);
                 float neighbor_radius = generator.vo_op.radii[neighbor_index];
                 Gizmos.DrawLine(pA,pB);
                 Gizmos.DrawWireSphere(pB, neighbor_radius);
